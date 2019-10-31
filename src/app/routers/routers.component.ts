a8a568d9aeebbe73be208fcb8636e22628bcec70
Roman Oliynyk <oliynykroma@live.com>
2019-10-31 21:31:19 +0200

    route iteration

--- src/app/routers/routers.component.ts
+++ src/app/routers/routers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-routers',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./routers.component.scss']
 })
 export class RoutersComponent implements OnInit {
-
-  constructor() { }
+  public id;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    console.log(this.id);
   }
 
 }
