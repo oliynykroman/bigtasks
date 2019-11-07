b21e341b348350f3026174a3bec360da5fa20815
Roman Oliynyk <oliynykroma@live.com>
2019-11-07 13:59:16 +0200

    checked for track by

--- src/app/pages/for/for.component.ts
+++ src/app/pages/for/for.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./for.component.scss']
 })
 export class ForComponent implements OnInit {
-
+  public heroes = [
+    { id: 1, name: 'Superman' },
+    { id: 2, name: 'Batman' },
+    { id: 5, name: 'BatGirl' },
+    { id: 3, name: 'Robin' },
+    { id: 4, name: 'Flash' }
+  ];
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  public otherArrayHero() {
+    this.heroes = [
+      { id: 1, name: 'Superman' },
+      { id: 2, name: 'Batman' },
+      { id: 5, name: 'BatGirl' },
+      { id: 3, name: 'Robin' },
+      { id: 4, name: 'Flash' },
+      { id: 4, name: 'Flash2' },
+    ]
+ }
+
+  public trackHero(index, hero) {
+    console.log(hero);
+    return hero ? hero.id : undefined;
+
+  }
 }
